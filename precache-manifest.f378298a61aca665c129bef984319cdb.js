0d8d98072b2c2de054db7904027635f91ede31e6
Karen Zhou <zz2202@nyu.edu>
2020-01-04 21:37:53 +0800

    Updates

---
+++ precache-manifest.f378298a61aca665c129bef984319cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610428516da0658cca5aa89bad8dbaae",
+    "url": "/monsters-rolodex-Udemy-practice-/index.html"
+  },
+  {
+    "revision": "5a84dba8ad9d6813059d",
+    "url": "/monsters-rolodex-Udemy-practice-/static/css/main.c89762d9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex-Udemy-practice-/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-Udemy-practice-/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a84dba8ad9d6813059d",
+    "url": "/monsters-rolodex-Udemy-practice-/static/js/main.39fe7cdb.chunk.js"
+  },
+  {
+    "revision": "3dd25b86bfa6443291e7",
+    "url": "/monsters-rolodex-Udemy-practice-/static/js/runtime-main.891a8cca.js"
+  }
+]);
